5e237fd58aaef978005fb3c620bd3c214d9dc4f3
Jean Zombie <22709399+Jean-Zombie@users.noreply.github.com>
2021-10-13 16:27:22 +0200

    Change staticfile storage

--- config/settings/production.py
+++ config/settings/production.py
@@ -59,7 +59,8 @@ SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
 
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+
 # MEDIA
 # ------------------------------------------------------------------------------
 
