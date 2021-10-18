7142a4432867976bd8d1e8f9655917705367f503
Jean Zombie <22709399+Jean-Zombie@users.noreply.github.com>
2021-10-18 13:52:23 +0200

    Add more domains to cors whitelist

--- config/settings/base.py
+++ config/settings/base.py
@@ -327,6 +327,7 @@ CORS_ALLOWED_ORIGINS = [
     "https://headless-wagtail-giinn.ondigitalocean.app",
     "http://localhost:8000",
     "http://localhost:3000",
+    "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
 
 ]
