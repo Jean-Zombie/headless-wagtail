caf91acc253a3463d4c95faafc427032327a7c1c
Jean Zombie <22709399+Jean-Zombie@users.noreply.github.com>
2021-10-18 20:05:34 +0200

    Revert to a80b6e52590d3d9a2f74b83c2b9c4fa741308a3a

---
+++ frontend/src/APIService.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+const API_ROOT = "http://localhost:8000/api/v2";
+
+export class APIService {
+  constructor() {}
+  async getPages(query) {
+    const url = `${API_ROOT}/pages/${query}`;
+    return axios.get(url).then(response => response.data);
+  }
+}
