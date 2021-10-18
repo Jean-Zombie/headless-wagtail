936c66d69528c3b2057676658bbc3fd752b3930b
Jean Zombie <22709399+Jean-Zombie@users.noreply.github.com>
2021-10-18 12:15:15 +0200

    Drop fetch for axios

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -25,7 +25,7 @@
       <v-col class="mb-5" cols="12">
         <h2 class="headline font-weight-bold">What's next?</h2>
         <v-row justify="center" v-for="(item, i) in news" :key="i">
-          <router-link class="" :to="/news/+ item.id">
+          <router-link class="" :to="/news/ + item.id">
             <h2>{{ item.title }}</h2>
           </router-link>
         </v-row>
@@ -35,7 +35,8 @@
 </template>
 
 <script>
-const API_ROOT = "http://127.0.0.1:8000/api/v2/pages/";
+import { APIService } from "../APIService.js";
+const apiService = new APIService();
 
 export default {
   name: "HelloWorld",
@@ -44,10 +45,9 @@ export default {
     news: {},
   }),
   async mounted() {
-    let response = await fetch(
-      API_ROOT + "?type=news.NewsPage&fields=intro,body,date,image_thumbnail"
-    ).then((response) => response.json());
-    this.news = response.items;
+    let response = await apiService
+      .getPages("?type=news.NewsPage&fields=intro,body,date,image_thumbnail")
+      .then((response) => (this.news = response.items));
   },
 };
 </script>
