28008f7c42a2ae73e8e42ae9d63b561051a81490
Jean Zombie <22709399+Jean-Zombie@users.noreply.github.com>
2021-10-20 09:54:05 +0200

    Add comment

--- config/settings/base.py
+++ config/settings/base.py
@@ -330,7 +330,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
 
 ]
-# CORS_ALLOW_ALL_ORIGINS: True
+# CORS_ALLOW_ALL_ORIGINS: True  
 
 # Your stuff...
 WAGTAIL_SITE_NAME = "headless_wagtail"
