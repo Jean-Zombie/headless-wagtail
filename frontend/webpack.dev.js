d05a90bbbcd4866dc7ba9f194968d4e71a74bb9e
Jean Zombie <22709399+Jean-Zombie@users.noreply.github.com>
2021-10-20 12:40:49 +0200

    Remove CORS options

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -7,11 +7,6 @@ module.exports = merge(common, {
   devServer: {
     port: 3000,
     hot: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*",
-      // "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      // "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-    }
     // watchOptions: { ignored: /node_modules/ }
-  }
+  },
 });
