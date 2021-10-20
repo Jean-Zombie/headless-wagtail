a3ffe2047cfc12da66e7b5226fc22378519424b1
Jean Zombie <22709399+Jean-Zombie@users.noreply.github.com>
2021-10-20 14:48:29 +0200

    Passing envs via cli, unifying webpack config files

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -5,51 +5,60 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const VueLoaderPlugin = require("vue-loader/lib/plugin");
 const path = require("path");
 const DEST_FOLDER = "dist";
-const Dotenv = require('dotenv-webpack');
+const Dotenv = require("dotenv-webpack");
 
-module.exports = {
-  mode: "production",
-  entry: { app: "./src/main.js" },
-  output: {
-    path: path.join(__dirname, DEST_FOLDER),
-    publicPath: "/static/dist/",
-    filename: "[name].[hash].bundle.js"
-  },
-  plugins: [
-    new BundleTracker({ filename: "./webpack-stats.json" }),
-    new CleanWebpackPlugin(),
-    new VueLoaderPlugin(),
-    new VuetifyLoaderPlugin({ progressiveImages: true }),
-    new Dotenv({
-      systemvars: true,
-    })
-  ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          chunks: "initial",
-          name: "vendor"
+module.exports = env => {
+  return {
+    entry: {
+      app: "./src/main.js"
+    },
+    output: {
+      path: path.join(__dirname, DEST_FOLDER),
+      publicPath: "/static/dist/",
+      filename: "[name].[hash].bundle.js"
+    },
+    plugins: [
+      new BundleTracker({ filename: "./webpack-stats.json" }),
+      new CleanWebpackPlugin(),
+      new VueLoaderPlugin(),
+      new VuetifyLoaderPlugin({ progressiveImages: true }),
+      new Dotenv({
+        path: `./.env.${env.enviroment}`
+      })
+    ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          commons: {
+            test: /[\\/]node_modules[\\/]/,
+            chunks: "initial",
+            name: "vendor"
+          }
         }
       }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader"
+          ]
+        },
+        { test: /\.vue$/, use: "vue-loader" },
+        { test: /.*\.(png|jpe?g)$/i, use: "file-loader" }
+      ]
+    },
+    output: { publicPath: "http://localhost:3000/static/dist/" },
+    devServer: {
+      port: 3000,
+      hot: true
+      // watchOptions: { ignored: /node_modules/ }
     }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          // Creates `style` nodes from JS strings
-          "style-loader",
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader"
-        ]
-      },
-      { test: /\.vue$/, use: "vue-loader" },
-      { test: /.*\.(png|jpe?g)$/i, use: "file-loader" }
-    ]
-  }
+  };
 };
